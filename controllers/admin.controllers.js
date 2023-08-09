ac78daa536c8e34563cb78d0132fcd3bd79dcc0a
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-08-09 02:26:56 -0300

    Se realizó el enrutador, controlador y vista para poder ver los datos de un paciente y poder modificarlo

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -137,7 +137,81 @@ crear_pacientes = (req, res) => {
   )  
 }
 
+editar_pacientes = (req, res) => {
+  const paciente_id = req.params.editar_paciente;
 
+  if (req.session.loggedin) {
+    conexion.query(
+      "SELECT * FROM `pacientes` LEFT JOIN personas ON pacientes.id_persona = personas.persona_id WHERE paciente_id = ? ",
+      [paciente_id],
+      (error, results) => {
+        res.render("administrador/editar_paciente", {
+          results: results[0],
+          login: true,
+        });
+      }
+    );
+  } else {
+    res.render("inicio_sesion/index", {
+      alert: true,
+      alertTitle: "Fallo",
+      alertMessage: "No ha iniciado sesión",
+      alertIcon: "error",
+      showConfirmButton: false,
+      timer: 1500,
+      ruta: "inicio_sesion",
+      login: false,
+    });
+  }
+};
+
+modificar_pacientes = (req, res) => {
+  
+  const {nombre,apellido,fecha_nac,direccion, dni,email, sexo, telefono, discapacidad,nombre_usuario,password} = req.body;
+  const rol = 1;
+
+  conexion.query(
+    "INSERT INTO personas SET ?",
+    {
+      nombres: nombre,
+      apellidos: apellido,
+      dni: dni,
+      direccion: direccion,
+      email: email,
+      fecha_nac: fecha_nac,
+      sexo: sexo,
+      telefono: telefono,
+      rol: rol,
+    },
+    (error, results) => {
+      if (error) {
+        throw error;
+      }else{
+        const id = results['insertId']; 
+
+        conexion.query("INSERT INTO usuarios SET ?",{
+          nombre_usuario: nombre_usuario,
+          contrasenia: password,
+          id_persona: id,
+        });
+
+        conexion.query("INSERT INTO pacientes SET ?",{
+          id_persona: id,
+          id_discapacidad: discapacidad,
+        });
+        res.render("administrador/index",{
+          alert: true,
+          alertTitle: "Exitoso",
+          alertMessage: "Registración Exitosa",
+          alertIcon: "success",
+          showConfirmButton: false,
+          timer: 1500,
+          ruta: "admin_pacientes"
+        })
+      }
+    }
+  )  
+}
 
 
 
@@ -146,5 +220,7 @@ module.exports = {
   admin_pacientes,
   eliminar_pacientes,
   agregar_pacientes,
-  crear_pacientes
+  crear_pacientes,
+  editar_pacientes,
+  modificar_pacientes
 };
