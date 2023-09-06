32fe61f4e2bd6d7316d2da8044c5c8fb72ed7dce
Carlos Ezequiel Romero <ezequielromero3039@gmail.com>
2023-09-06 16:03:38 -0300

    Se agregó el archivo app.js

---
+++ app.js
@@ -0,0 +1,60 @@
+// Importaciones de librerías
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+
+// El método config de dotenv permite leer variables de entorno desde un archivo .env
+require("dotenv").config();
+
+// Si no existe el archivo .env, el valor por defecto del puerto será 6000
+const port = process.env.PORT;
+
+// Se inicializa express para poder usar sus métodos
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Archivos estáticos utilizando la librería path que viene en NodeJS
+app.use(express.static(path.join(__dirname, "public")));
+
+// Configuración de motor de plantillas EJS
+app.set("view engine", "ejs");
+
+//Se conecta la BD
+const { conectarDB } = require("./conection/db");
+conectarDB();
+
+app.use(require("./routers/index.routes"));
+app.use(require("./routers/auth.routes"));
+app.use(require("./routers/pacientes.routes"));
+app.use(require("./routers/admin.routes"));
+app.use(require("./routers/usuario.routes"));
+app.use(require("./routers/persona.routes"));
+app.use(require("./routers/doctores.routes"));
+app.use(require("./routers/horarios.routes"));
+app.use(require("./routers/turnos.routes"));
+app.use(require("./routers/especialidades.routes"));
+
+// 404 - Not found
+app.use((req, res, next) => {
+  res.write(`<div>
+        <h1>404 - Ruta no encontrada</h1>
+        <hr>
+        <p>La pagina que intentas buscar no existe</p>
+        <p>Redireccionando a la página de inicio...</p>
+        <script>
+        (
+          () => setTimeout(() => {
+            window.location.href='http://localhost:${port}/';
+           }, 3000)           
+        )();
+        </script>
+    </h1>`);
+});
+
+// Servidor en escucha de peticiones
+app.listen(port, console.log(`Servidor corriendo en http://localhost:${port}`));
